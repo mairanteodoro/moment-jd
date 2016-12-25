21e2654a208e2770dd9e1335f238a096aa20e1b7
mairanteodoro <mairan.teodoro@gmail.com>
2016-12-25 13:04:44 -0500

    Implement fromJD() method

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+/*
+  Import plugin.
+  Note that jQuery & Moment.js have already been loaded via webpack.config.js ProvidePlugin method. This is the same as adding script tags into the index.html file (in the right sequence, of course).
+*/
 import './moment-jd';
 
 var date = moment.utc();
 var jd = moment.fn.toJD(date);
+// moment.fn.fromJD() will return UNIX time
+var convertedDate = moment.fn.fromJD(jd).toISOString();
 
 $(function(){
 
   $('#date').html('Current ISO-8601: ' + date.toISOString());
   $('#jd').html('Corresponding JD: ' + jd);
+  $('#convertedDate').html('Date converted from JD: ' + convertedDate);
 
 });
--- moment-jd.js
+++ moment-jd.js
@@ -70,95 +70,79 @@
     return jd;
   }
 
+  moment.fn.fromJD = (myJD) => {
+    /*
+      Convert a given Julian Date into Gregorian Date.
+
+      The returned value is a Moment.js object.
+
+      Example:
+      > jd = 2299159.1414699075
+      > date = moment.convertFromJD( jd )
+      > date.year() (output: 1582)
+      > date.hour() (output: 15 )
+    */
+
+    // Julian calendar ends on 1582 Oct 05.
+    // Dates prior or equal to 1582 Oct 04 @ 23:59:59
+    // correspond to JD <= 2299160.49999...
+    if (myJD <= 2299160.5) {
+      // Julian calendar
+      var b = 0;
+      var c = myJD + 32082;
+    } else {
+      // Gregorian calendar
+      var a = myJD + 32044;
+      var b = Math.floor((4 * a + 3) / 146097);
+      var c = a - Math.floor((146097 * b) / 4);
+    }
+    var d = Math.floor((4 * c) / 1461);
+    var e = c - Math.floor((1461 * d) / 4);
+    var m = Math.floor((5 * e + 2) / 153);
+    var myDay = e - Math.floor((153 * m + 2) / 5) + 1;
+    var myMonth = m + 3 - 12 * Math.floor(m / 10);
+    var myYear = 100 * b + d - 4800 + Math.floor(m / 10);
+    // deal with time
+    var myHour, myMinute, mySecond, myMillisecond;
+    if (myDay % 1 !== 0) {
+      //
+      // using 24 h format (00:00:00)
+      //
+      if (myDay % 1 === 0.5) {
+        // it is midnight; bubble up the day
+        myDay = Math.floor(myDay) + 1;
+        myHour = 0;
+        myMinute = 0;
+        mySecond = 0;
+      } else {
+        myHour = Math.floor((myDay % 1) < 0.5 ?
+        // (myDay % 1) <= 0.5 corresponds to afternoon + night (i.e. 12:00:01-23:59:59)
+                            (myDay % 1) * 24 + 12 :
+        // (myDay % 1) > 0.5 corresponds to the "wee" hours + morning (i.e. 00:00:01-11:59:59)
+                            (myDay % 1) * 24 - 12);
+        myMinute = ((myDay % 1) * 1440 - ((myDay % 1) * 1440) % 1) % 60;
+        mySecond = ((myDay % 1) * 86400 - ((myDay % 1) * 86400) % 1) % 60;
+        myDay = myDay - myDay % 1;
+      }
+    } else {
+      // it is noon
+      myHour = 12;
+      myMinute = 0;
+      mySecond = 0;
+    }
+
+    return moment.utc(
+                      [myYear,
+                      // keep Moment.js format for
+                      // months, i.e., 0->Jan; 1->Feb...
+                      myMonth - 1,
+                      myDay,
+                      myHour,
+                      myMinute,
+                      mySecond]
+    );
+  }
+
   return moment;
-
-  // function convertToJD(myDate) {
-  //   /*
-  //     Convert a Moment.js object into a Julian Date.
-  //     N.B.: Julian Date = Julian Day Number + Time
-  //
-  //     Example:
-  //     > date = moment.utc([1582, 9, 3, 15, 23, 43])
-  //     > jd = moment.convertToJD(date)
-  //     (console.log(jd) outputs 2299159.1414699075)
-  //   */
-  //   // return the result from the method implemented above
-  //   return moment.utc(myDate).toJD();
-  // }
-  //
-  // function convertFromJD(myJD) {
-  //   /*
-  //     Convert a given Julian Date into Gregorian Date.
-  //
-  //     The returned value is a Moment.js object.
-  //
-  //     Example:
-  //     > jd = 2299159.1414699075
-  //     > date = moment.convertFromJD( jd )
-  //     > date.year() (output: 1582)
-  //     > date.hour() (output: 15 )
-  //   */
-  //
-  //   // Julian calendar ends on 1582 Oct 05.
-  //   // Dates prior or equal to 1582 Oct 04 @ 23:59:59
-  //   // correspond to JD <= 2299160.49999...
-  //   if (myJD <= 2299160.5) {
-  //     // Julian calendar
-  //     var b = 0;
-  //     var c = myJD + 32082;
-  //   } else {
-  //     // Gregorian calendar
-  //     var a = myJD + 32044;
-  //     var b = Math.floor((4 * a + 3) / 146097);
-  //     var c = a - Math.floor((146097 * b) / 4);
-  //   }
-  //   var d = Math.floor((4 * c) / 1461);
-  //   var e = c - Math.floor((1461 * d) / 4);
-  //   var m = Math.floor((5 * e + 2) / 153);
-  //   var myDay = e - Math.floor((153 * m + 2) / 5) + 1;
-  //   var myMonth = m + 3 - 12 * Math.floor(m / 10);
-  //   var myYear = 100 * b + d - 4800 + Math.floor(m / 10);
-  //   // deal with time
-  //   var myHour, myMinute, mySecond, myMillisecond;
-  //   if (myDay % 1 !== 0) {
-  //     //
-  //     // using 24 h format (00:00:00)
-  //     //
-  //     if (myDay % 1 === 0.5) {
-  //       // it is midnight; bubble up the day
-  //       myDay = Math.floor(myDay) + 1;
-  //       myHour = 0;
-  //       myMinute = 0;
-  //       mySecond = 0;
-  //     } else {
-  //       myHour = Math.floor((myDay % 1) < 0.5 ?
-  //       // (myDay % 1) <= 0.5 corresponds to afternoon + night (i.e. 12:00:01-23:59:59)
-  //                           (myDay % 1) * 24 + 12 :
-  //       // (myDay % 1) > 0.5 corresponds to the "wee" hours + morning (i.e. 00:00:01-11:59:59)
-  //                           (myDay % 1) * 24 - 12);
-  //       myMinute = ((myDay % 1) * 1440 - ((myDay % 1) * 1440) % 1) % 60;
-  //       mySecond = ((myDay % 1) * 86400 - ((myDay % 1) * 86400) % 1) % 60;
-  //       myDay = myDay - myDay % 1;
-  //     }
-  //   } else {
-  //     // it is noon
-  //     myHour = 12;
-  //     myMinute = 0;
-  //     mySecond = 0;
-  //   }
-  //
-  //   return moment.utc(
-  //                     [myYear,
-  //                     // keep Moment.js format for
-  //                     // months, i.e., 0->Jan; 1->Feb...
-  //                     myMonth - 1,
-  //                     myDay,
-  //                     myHour,
-  //                     myMinute,
-  //                     mySecond]
-  //   );
-  // }
-
-  //
 
 })(moment);
